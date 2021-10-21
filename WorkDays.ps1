9d39f8ee9ed78a75bf4d175780826371073b720f
Des Finkenzeller <nutsoft@hotmail.com>
2021-10-21 19:21:47 +0100

    Implemented days between two dates function and tests

--- WorkDays.ps1
+++ WorkDays.ps1
@@ -89,22 +89,22 @@ function Get-EasterSunday {
 
     process {
         $obj = '' |
-            Add-Member -PassThru -MemberType NoteProperty   -Name Year  -Value $Year |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C7    -Value { $this.Year%19 } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C8    -Value { [System.Math]::Truncate($this.Year/100) } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C9    -Value { $this.Year%100 } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C10   -Value { [System.Math]::Truncate($this.C8/4) } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C11   -Value { $this.C8%4 } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C12   -Value { [System.Math]::Truncate($this.C8+8)/25 } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C13   -Value { [System.Math]::Truncate(($this.C8-$this.C12+1)/3) } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C14   -Value { ((19*$this.C7)+$this.C8-$this.C10-$this.C13+15)%30 } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C15   -Value { [System.Math]::Truncate($this.C9/4) } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C16   -Value { $this.C9%4 } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C17   -Value { (32+2*($this.C11+$this.C15)-$this.C14-$this.C16)%7 } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C18   -Value { [System.Math]::Truncate(($this.C7+(11*$this.C14)+(22*$this.C17))/451) } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name Month -Value { [System.Math]::Truncate(($this.C14+$this.C17-(7*$this.C18)+114)/31) } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name C20   -Value { ($this.C14+$this.C17-(7*$this.C18)+114)%31 } |
-            Add-Member -PassThru -MemberType ScriptProperty -Name Day   -Value { $this.C20+1 }
+        Add-Member -PassThru -MemberType NoteProperty   -Name Year  -Value $Year |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C7    -Value { $this.Year % 19 } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C8    -Value { [System.Math]::Truncate($this.Year / 100) } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C9    -Value { $this.Year % 100 } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C10   -Value { [System.Math]::Truncate($this.C8 / 4) } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C11   -Value { $this.C8 % 4 } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C12   -Value { [System.Math]::Truncate($this.C8 + 8) / 25 } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C13   -Value { [System.Math]::Truncate(($this.C8 - $this.C12 + 1) / 3) } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C14   -Value { ((19 * $this.C7) + $this.C8 - $this.C10 - $this.C13 + 15) % 30 } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C15   -Value { [System.Math]::Truncate($this.C9 / 4) } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C16   -Value { $this.C9 % 4 } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C17   -Value { (32 + 2 * ($this.C11 + $this.C15) - $this.C14 - $this.C16) % 7 } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C18   -Value { [System.Math]::Truncate(($this.C7 + (11 * $this.C14) + (22 * $this.C17)) / 451) } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name Month -Value { [System.Math]::Truncate(($this.C14 + $this.C17 - (7 * $this.C18) + 114) / 31) } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name C20   -Value { ($this.C14 + $this.C17 - (7 * $this.C18) + 114) % 31 } |
+        Add-Member -PassThru -MemberType ScriptProperty -Name Day   -Value { $this.C20 + 1 }
 
         Get-Date ("{0}/{1}/{2}" -f $obj.Year, $obj.Month, $obj.Day)
     }
@@ -258,4 +258,47 @@ function Get-BankHolidays {
             (Get-BoxingDay -Year $Year)
         )
     }
+}
+
+function Get-DaysBetweenTwoDates {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Mandatory = $false
+        )]
+        [datetime]
+        $Start = (Get-Date),
+        [Parameter(
+            Mandatory = $false
+        )]
+        [ValidateScript({
+                if ($_ -lt $Start) {
+                    throw "End date must be after start date: $Start"
+                }
+                else {
+                    $true
+                }
+            })]
+        [datetime]
+        $End = (Get-Date),
+        [switch]
+        $Weekdays
+    )
+
+    if ($End.Year -gt $Start.Year) {
+        $bankHolidays = ($Start.Year)..($End.Year) | Get-BankHolidays
+    }
+    else {
+        $bankHolidays = Get-BankHolidays -Year $Start.Year
+    }
+
+    $day = $Start
+    $(
+        do {
+            $day
+            $day = $day.AddDays(1)
+        } while ($day -lt $End)
+    ) |
+    Where-Object { -not ($_.DayOfWeek -match 'Saturday|Sunday' -and $PSBoundParameters.ContainsKey('Weekdays')) } |
+    Where-Object { -not ($bankHolidays -contains $_ -and $PSBoundParameters.ContainsKey('Weekdays')) }
 }
