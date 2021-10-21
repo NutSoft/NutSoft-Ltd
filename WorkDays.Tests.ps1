912cad9094badf54c23a232ef8a11a52ba4955a8
Des Finkenzeller <nutsoft@hotmail.com>
2021-10-21 16:55:42 +0100

    Expanded Pester tests to include 2020 and 2022

--- WorkDays.Tests.ps1
+++ WorkDays.Tests.ps1
@@ -18,6 +18,35 @@ Describe "WorkDays" {
         }
     }
     Context "UK Public Holidays" {
+        Context "2020" {
+            It "New Year's Day 2020" {
+                Get-NewYearsDay -Year 2020 | Should -Be (Get-Date 1/1/2020)
+            }
+            It "Good Friday" {
+                Get-GoodFriday -Year 2020 | Should -Be (Get-Date 10/4/2020)
+            }
+            It "Easter Sunday" {
+                Get-EasterSunday -Year 2020 | Should -Be (Get-Date 12/4/2020)
+            }
+            It "Easter Monday" {
+                Get-EasterMonday -Year 2020 | Should -Be (Get-Date 13/4/2020)
+            }
+            It "Early May Bank Holiday" {
+                Get-EarlyMayBankHoliday -Year 2020 | Should -Be (Get-Date 8/5/2020)
+            }
+            It "Spring Bank Holiday" {
+                Get-SpringBankHoliday -Year 2020 | Should -Be (Get-Date 25/5/2020)
+            }
+            It "Summer Bank Holiday" {
+                Get-SummerBankHoliday -Year 2020 | Should -Be (Get-Date 31/8/2020)
+            }
+            It "Christmas Day" {
+                Get-ChristmasDay -Year 2020 | Should -Be (Get-Date 25/12/2020)
+            }
+            It "Boxing Day" {
+                Get-BoxingDay -Year 2020 | Should -Be (Get-Date 28/12/2020)
+            }
+        }
         Context "2021" {
             It "New Year's Day 2021" {
                 Get-NewYearsDay -Year 2021 | Should -Be (Get-Date 1/1/2021)
@@ -45,6 +74,35 @@ Describe "WorkDays" {
             }
             It "Boxing Day" {
                 Get-BoxingDay -Year 2021 | Should -Be (Get-Date 28/12/2021)
+            }
+        }
+        Context "2022" {
+            It "New Year's Day 2022" {
+                Get-NewYearsDay -Year 2022 | Should -Be (Get-Date 3/1/2022)
+            }
+            It "Good Friday" {
+                Get-GoodFriday -Year 2022 | Should -Be (Get-Date 15/4/2022)
+            }
+            It "Easter Sunday" {
+                Get-EasterSunday -Year 2022 | Should -Be (Get-Date 17/4/2022)
+            }
+            It "Easter Monday" {
+                Get-EasterMonday -Year 2022 | Should -Be (Get-Date 18/4/2022)
+            }
+            It "Early May Bank Holiday" {
+                Get-EarlyMayBankHoliday -Year 2022 | Should -Be (Get-Date 2/5/2022)
+            }
+            It "Spring Bank Holiday" {
+                Get-SpringBankHoliday -Year 2022 | Should -Be (Get-Date 2/6/2022)
+            }
+            It "Summer Bank Holiday" {
+                Get-SummerBankHoliday -Year 2022 | Should -Be (Get-Date 29/8/2022)
+            }
+            It "Christmas Day" {
+                Get-ChristmasDay -Year 2022 | Should -Be (Get-Date 26/12/2022)
+            }
+            It "Boxing Day" {
+                Get-BoxingDay -Year 2022 | Should -Be (Get-Date 27/12/2022)
             }
         }
     }
